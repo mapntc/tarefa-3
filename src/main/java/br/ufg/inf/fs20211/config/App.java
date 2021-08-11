8c7c495b10498b541cdcd3da4a4b4d0cfa9d21de
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-08-11 02:33:40 -0300

    Adiciona CRUD de Hospede e Hospedagem

--- src/main/java/br/ufg/inf/fs20211/config/App.java
+++ src/main/java/br/ufg/inf/fs20211/config/App.java
@@ -1,24 +1,23 @@
 package br.ufg.inf.fs20211.config;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
+import java.time.LocalDate;
 
 import br.ufg.inf.fs.enums.CategoriaQuarto;
+import br.ufg.inf.fs20211.ctrl.HospedagemCtrl;
+import br.ufg.inf.fs20211.ctrl.HospedeCtrl;
 import br.ufg.inf.fs20211.ctrl.HotelCtrl;
 import br.ufg.inf.fs20211.ctrl.QuartoCtrl;
+import br.ufg.inf.fs20211.entities.Hospedagem;
+import br.ufg.inf.fs20211.entities.Hospede;
 import br.ufg.inf.fs20211.entities.Hotel;
 import br.ufg.inf.fs20211.entities.Quarto;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Começando");
-
-		testeJpaQuarto();
-
+		System.out.println("ComeÃ§ando");
+		testeJpaHospedagem();
+		System.out.println("Finaliazdo.");
 	}
 	
 	public static void testeJpaQuarto() {
@@ -93,79 +92,64 @@ public class App {
 		ctrl.delete(hotel.getIdHotel());
 		System.out.println("Terminou");
 	}
-
-	public static void testeJpa() {
-		EntityManager em = DaoFactory.getEntityManager();
+	
+	// ImplementaÃ§Ã£o
+	
+	public static void testeJpaHospede() {
+		HospedeCtrl ctrl = new HospedeCtrl();
 		
-		// CREATE
-		/*//Hotel hotel = new Hotel(null, "Castro`s Park Hotel", "Goiânia", 5);
-		em.getTransaction().begin();
-		//em.persist(hotel);
-		Hotel h1 = new Hotel(null, "Ibis", "Goiânia", 3);
-		Hotel h2 = new Hotel(null, "Copacabana Palace", "Rio de Janeiro", 5);
-		Hotel h3 = new Hotel(null, "Denali Hotel", "Anápolis", 4);
+		System.out.println("Inserir no banco");
+		Hospede hospede = new Hospede("Elias", LocalDate.of(2001, 03, 9), 123);
+		hospede = ctrl.insert(hospede);
+		System.out.println(hospede);
 		
-		em.persist(h1);
-		em.persist(h2);
-		em.persist(h3);
-		
-		
-		em.getTransaction().commit();
-		*/
-		
-		// READ ALL
-		/*
-		System.out.println("Buscar Todos");
-		TypedQuery<Hotel> result = em.createQuery("from Hotel", Hotel.class);
-		List<Hotel> hoteis = result.getResultList();
-		for(Hotel h : hoteis) {
+		System.out.println("Busca Todos");
+		for(Hospede h : ctrl.findAll()) {
 			System.out.println(h);
 		}
 		
+		System.out.println("Buscar por ID");
+		System.out.println(ctrl.findById(1));
 		
-		// READ BY ID
-		Hotel h4 = em.find(Hotel.class, 3);
-		
-		System.out.println("Buscar Por ID");
-		System.out.println(h4);
-		
-		System.out.println("Buscar por Ocorrência");
-		String sql = "from Hotel h where h.nmHotel like :str";
-		TypedQuery<Hotel> result2 = em.createQuery(sql, Hotel.class);
-		result2.setParameter("str", "%i%");
-		hoteis = result2.getResultList();
-		for(Hotel h : hoteis) {
-			System.out.println(h);
-		}*/
-		
-		System.out.println("Update do Objeto");
-		Hotel hotelUpdate = em.find(Hotel.class, 2);
-		
-		hotelUpdate.setNmHotel("Ibis Hotel");
-		
-		em.getTransaction().begin();
-		em.persist(hotelUpdate);
-		em.getTransaction().commit();
+		System.out.println("Alterar no banco");
+		hospede.setNmHospede("Elias Alterado");
+		hospede = ctrl.update(hospede);
+		System.out.println(hospede);
 		
 		
-		
-		System.out.println("Delete do Objeto");
-		/*new Hotel(null, "Hotel Santo Antônio", "Neropolis", 3);
-		System.out.println(hotelDeleta);
-		System.out.println("persistindo");
-		em.getTransaction().begin();
-		em.persist(hotelDeleta);
-		em.getTransaction().commit();
-		System.out.println("novo objeto");
-		System.out.println(hotelDeleta);*/
-		/*Hotel hotelDeleta = em.find(Hotel.class, 5);
-		
-		em.getTransaction().begin();
-		em.remove(hotelDeleta);
-		em.getTransaction().commit();
-		*/
-		
-		DaoFactory.closeConnection();
+		System.out.println("Excluir");
+		ctrl.delete(hospede.getIdHospede());
+		System.out.println("Terminou");
 	}
 
+	public static void testeJpaHospedagem() {
+		HospedagemCtrl ctrl = new HospedagemCtrl();
+		QuartoCtrl ctrlQuarto = new QuartoCtrl();
+		Quarto quarto = ctrlQuarto.findById(1);
+		HospedeCtrl ctrlHospede = new HospedeCtrl();
+		Hospede hospede = ctrlHospede.findById(1);
+		
+		System.out.println("Inserir no banco");
+		Hospedagem hospedagem = new Hospedagem(quarto, hospede, LocalDate.of(2021,8,10), LocalDate.of(2021,8,12));
+		hospedagem = ctrl.insert(hospedagem);
+		System.out.println(hospedagem);
+		
+		System.out.println("Busca Todos");
+		for(Hospedagem h : ctrl.findAll()) {
+			System.out.println(h);
+		}
+		
+		System.out.println("Buscar por ID");
+		System.out.println(ctrl.findById(1));
+		
+		System.out.println("Alterar no banco");
+		hospedagem.setDtCheckout(LocalDate.of(2021, 8, 20));
+		hospedagem = ctrl.update(hospedagem);
+		System.out.println(hospedagem);
+		
+		
+		System.out.println("Excluir");
+		ctrl.delete(hospedagem.getIdHospedagem());
+		System.out.println("Terminou");
+	}
 }
